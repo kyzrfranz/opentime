cd7545e58bdf1318f578eff243236db8148402e1
Thomas Hieber <thomas.hieber@rewe-digital.com>
2018-04-24 20:25:41 +0200

    added skeleton & first tests

--- backend/src/main/java/de/kyzrfranz/opentime/be/OpentimeBeApplication.java
+++ backend/src/main/java/de/kyzrfranz/opentime/be/OpentimeBeApplication.java
@@ -1,12 +1,20 @@
 package de.kyzrfranz.opentime.be;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.AutoConfigureAfter;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.embedded.EmbeddedMongoAutoConfiguration;
+import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { MongoAutoConfiguration.class, MongoDataAutoConfiguration.class })
+@EnableReactiveMongoRepositories
+@AutoConfigureAfter(EmbeddedMongoAutoConfiguration.class)
 public class OpentimeBeApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(OpentimeBeApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(OpentimeBeApplication.class, args);
+    }
+
 }
